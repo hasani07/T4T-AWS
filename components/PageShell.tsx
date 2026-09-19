bb787ccaee605f7a41e0e679c0d9ab8c93666424
hasani07 <74604665+hasani07@users.noreply.github.com>
2026-09-19 16:57:58 +0700

    Update PageShell.tsx

--- components/PageShell.tsx
+++ components/PageShell.tsx
@@ -1,7 +1,7 @@
 export default function PageShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-[#F1F0F7] px-4 pb-24 pt-6 md:pb-10 md:pl-28 md:pr-8 md:pt-8">
-      <div className="mx-auto max-w-6xl">{children}</div>
+      <div className="mx-auto max-w-[1600px]">{children}</div>
     </div>
   );
 }
