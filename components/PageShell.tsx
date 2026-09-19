e56a3f476a5743a660da3dc1250cfbc2fadd086a
hasani07 <74604665+hasani07@users.noreply.github.com>
2026-09-20 01:10:58 +0700

    Update PageShell.tsx

--- components/PageShell.tsx
+++ components/PageShell.tsx
@@ -1,6 +1,6 @@
 export default function PageShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#F1F0F7] px-4 pb-24 pt-6 md:pb-10 md:pl-28 md:pr-8 md:pt-8">
+    <div className="min-h-screen bg-[#F1F0F7] px-4 pb-[calc(6rem+env(safe-area-inset-bottom,0px))] pt-6 md:pb-10 md:pl-28 md:pr-8 md:pt-8">
       <div className="w-full">{children}</div>
     </div>
   );
